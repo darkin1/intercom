349b97a926cfa809f9efdf7774814f740c247475
Gabriel Passarelli <gabriel@descubraomundo.com>
2016-07-28 18:43:13 -0300

    Change from getevn() to env()

--- src/IntercomServiceProvider.php
+++ src/IntercomServiceProvider.php
@@ -1,4 +1,4 @@
-<?php
+    <?php
 namespace Darkin1\Intercom;
 
 use Illuminate\Support\ServiceProvider;
@@ -16,13 +16,13 @@ class IntercomServiceProvider extends ServiceProvider
     public function register()
     {
         // Don't register intercom if it is not configured.
-        if (! getenv('INTERCOM_APP_ID') && ! getenv('INTERCOM_APP_KEY') && ! $this->app['config']->get('services.intercom')) {
+        if (! env('INTERCOM_APP_ID') && ! env('INTERCOM_APP_KEY') && ! $this->app['config']->get('services.intercom')) {
             return;
         }
 
         $this->app->bind('intercom', function () {
-            $appID  = getenv('INTERCOM_APP_ID')  ?: $app['config']->get('services.intercom.app_id');
-            $apiKey = getenv('INTERCOM_APP_KEY') ?: $app['config']->get('services.intercom.api_key');
+            $appID  = env('INTERCOM_APP_ID')  ?: $app['config']->get('services.intercom.app_id');
+            $apiKey = env('INTERCOM_APP_KEY') ?: $app['config']->get('services.intercom.api_key');
 
             return new IntercomClient($appID, $apiKey);
         });
