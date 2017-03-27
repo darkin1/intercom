9a7e3f52c936d12e0eda450a2b711315302362c7
dariusz <dciesielski87@gmail.com>
2017-03-27 10:55:26 +0200

    phpunit 6 backward compatibility

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -2,7 +2,6 @@
 
 // backward compatibility
 if (! class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
-echo "Test"; exit;
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
 
