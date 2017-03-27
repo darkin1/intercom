ce12c2737acc6909c91c68669ec78f467a59a5d4
Dariusz <dciesielski87@gmail.com>
2017-03-27 07:45:43 +0000

    Apply fixes from StyleCI

--- tests/HelloWorldTest.php
+++ tests/HelloWorldTest.php
@@ -1,9 +1,9 @@
 <?php
+
 // backward compatibility
-if (!class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
+if (! class_exists('\PHPUnit\Framework\TestCase') && class_exists('\PHPUnit_Framework_TestCase')) {
     class_alias('\PHPUnit_Framework_TestCase', '\PHPUnit\Framework\TestCase');
 }
-
 
 /**
  * Class HelloWorldTest.
